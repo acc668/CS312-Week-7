b684527bb1dd6c550ef7f0a6ebab23c74745e45b
acc668 <116805246+acc668@users.noreply.github.com>
2024-07-24 22:03:36 -0700

    Exercise - 2: Complete the Keeper Project

--- CreateArea.jsx
+++ CreateArea.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+  const [isExpanded, SetExpanded] = useState(false);
+
   const [note, setNote] = useState({
     title: "",
     content: "",
@@ -26,23 +31,34 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function expand() {
+    SetExpanded(true);
+  }
+
   return (
     <div>
-      <form>
-        <input
-          name="title"
-          placeholder="Title"
-          value={note.title}
-          onChange={handleChange}
-        />
+      <form className="create-note">
+        {isExpanded && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
         <textarea
           name="content"
-          placeholder="Take a note..."
-          rows="3"
-          value={note.content}
+          onClick={expand}
           onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={isExpanded ? 3 : 1}
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
